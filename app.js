25d03d8166fdf0a4b2600834f73f1c166cfcc820
Noah Greff <greffnoah@gmail.com>
2020-11-04 17:46:04 +0100

    commented app.js

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
+/**
+ * Loading all dependencies
+ */
+
 const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
 
+
+/**
+ * Creating new Express app.
+ */
 const app = express();
+
+
+/**
+ * Joining __dirname to /dist for heroku
+ */
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')));
 
+
+/**
+ * Deploying app on port 8080 or env.PORT 
+ */
+
 const port = process.env.PORT || 8080;
 app.listen(port);
+
+
+/**
+ * Simple deployment success feedback
+ */
 
 console.log('VUEJS app running on port: ' + port);
