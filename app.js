63c4157dc412ea06cac9edd6b1634199a0d31475
Noah Greff <greffnoah@gmail.com>
2020-11-20 12:35:22 +0100

    Testing automatic deploy

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
  */
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')));
-app.get(/.*/, (req,res) => res.sendFile(path.resolve(__dirname,'public/index.html')));
+app.get(/.*/, (req, res) => res.sendFile(path.resolve(__dirname,'public/index.html')));
 
 
 /**
