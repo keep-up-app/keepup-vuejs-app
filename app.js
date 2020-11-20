c7609cee338a015571c369ee1a34ed9969bc2ff7
Noah Greff <greffnoah@gmail.com>
2020-11-20 12:21:51 +0100

    adding callback routes for vuejs history mode

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const app = express();
  */
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')));
+app.get(/.*/, (req,res) => res.sendFile(path.resolve(__dirname,'public/index.html')));
 
 
 /**
